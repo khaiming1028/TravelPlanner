45e0e3a4f6d48e0f3e98d50caa1d5efc94a064dc
khaiming1028 <khaiming1028@gmail.com>
2026-06-04 14:41:14 +0800

    server setup

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json()); 
 app.use(cors());
-
+app.use(express.static('public'));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
